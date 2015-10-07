b306e4506d8e9597a0dcd229870206fe876df3f1
Upasana Dixit <dixit.upasanaitbhu@gmail.com>
2015-10-07 16:53:42 -0700

    Renamed the folder to DSGenericsClasses

---
+++ src/DSGenericClasses/TreeNode.java
@@ -0,0 +1,37 @@
+package DSHelperClasses;
+import java.lang.*;
+
+import java.util.Comparator;
+
+/**
+ * Created by Upasana on 10/6/2015.
+ * * Basic definition of a Node of a List
+ */
+public class TreeNode<T extends Comparable<T>> {
+    public T val;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
+    public TreeNode(T val)
+    {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+
+    public T GetValue()
+    {
+        return this.val;
+    }
+    public int CompareByVal(T data) {
+        return val.compareTo(data);
+
+    }
+
+    public int compareTo(Object o) {
+        TreeNode<T> currNode = (TreeNode) o;
+        return val.compareTo(currNode.val);
+
+    }
+
+}
